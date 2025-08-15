cf87c3ebcf8d06807e79ac0833433976999ea8ae
oliviawyy <olivia.c@aluno.senai.br>
2025-08-15 11:26:18 -0300

    gitignore

---
+++ 01_Hello_World/hello_world.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+#criando uma janela
+janela = tk.Tk()
+janela.title("Hello World")
+
+# criando um rótulo
+rotulo = tk.Label(janela, text="Olá, Mundo!")
+rotulo.pack()
+
+#loop para manter a janela aberta
+janela.mainloop()

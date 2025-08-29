669c4155b98c3708aa0fe42c43aa18ad90d00ff8
oliviawyy <olivia.c@aluno.senai.br>
2025-08-29 11:29:52 -0300

    haha

--- 03_bot_gemini/testando_api_gemini.py
+++ 03_bot_gemini/testando_api_gemini.py
@@ -4,6 +4,7 @@ from google import genai
 client = genai.Client(api_key="AIzaSyDec3s7AsDRXmaWDITmq3s-EjXmLUS1MrI")
 
 response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Explain how AI works in a few words"
+    model="gemini-2.5-flash", contents="quanto tempo os alunos de DS deveriam ter de intervalo?" \
+    "?"
 )
 print(response.text)

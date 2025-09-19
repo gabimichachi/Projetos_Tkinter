7d9f9bac10e20064f22d0dc1a4f81218b526c9b1
oliviawyy <olivia.c@aluno.senai.br>
2025-09-19 11:29:08 -0300

    LALALA

---
+++ 05_lista_de_tarefas/lista_tarefas.py
@@ -0,0 +1,106 @@
+import tkinter
+import ttkbootstrap as ttk
+import tkinter.messagebox as msg
+
+class Login():
+    def __init__(self):
+# criando a janela
+        self.janela = ttk.Window(themename= "minty",
+                                title="campo de login")
+        self.janela.geometry("800x600")
+
+#impede que o usuario redimensione a janela
+        self.janela.resizable(False, False)
+
+#mudando a cor de fundo da janela
+        self.janela.iconbitmap("05_lista_de_tarefas/cadeado.ico")
+
+#criando um rótulo
+        self.rotulo = ttk.Label(self.janela,
+                        text="Login",
+                        background="pink",
+                        foreground="black",
+                        font=("Impact", 20))
+        self.rotulo.pack(pady=30)
+
+#perguntar USUARIO
+        self.usuario = ttk.Label(self.janela,
+                      text="USUÁRIO: ",
+                      foreground="black",
+                      font=("Comic Sans MS", 12))
+        self.usuario.pack(pady=10)
+
+#digitar usuario
+        self.digitar = ttk.Entry(self.janela,
+                            text="",
+                                background="pink",
+                                foreground="pink",
+                                font=("Arial", 12),
+                                style="secondary")
+
+        self.digitar.pack(pady=2)
+
+#perguntar SENHA
+        self.senha = ttk.Label(self.janela,
+                      text="SENHA: ",
+                      foreground="black",
+                      font=("Comic Sans MS", 12))
+        self.senha.pack(pady=10)
+
+#digitar SENHA
+        self.digite = ttk.Entry(self.janela,
+                            text="",
+                            show="•",
+                                background="pink",
+                                foreground="pink",
+                                font=("Arial", 12),
+                                style="secondary")
+
+        self.digite.pack(pady=2)
+
+        self.frame_botao = ttk.Frame()
+        self.frame_botao.pack()
+
+ #criar botao
+        self.button_botao= ttk.Button(self.frame_botao,
+                                text="logar",
+                                style="secondary",
+                                padding= (20,10),
+                                command=self.logar
+                                )
+        self.button_botao.pack(side="left", padx=10, pady=20)
+
+        #criar botao
+        self.button_botao= ttk.Button(self.frame_botao,
+                                text="sair",
+                                padding= (20,10),
+                                style="secondary",
+                                command=self.janela.destroy
+                                )
+        self.button_botao.pack(side="right", padx=10, pady=20)
+
+    # def tarefas(self):
+    #     self.janela.destroy()
+    #     import lista_tarefas
+    #     app = lista_tarefas.Tarefas()
+        # app.run()
+
+    def logar(self):
+        usuario_senha = (self.digitar.get())
+        usuario_digitar = (self.digite.get())
+        if usuario_senha == "oliviawyy" and usuario_digitar == "1234":
+            tkinter.messagebox.showinfo(title= "Login realizado com sucesso!", message = "Bem Vindo!!")
+        else:
+            tkinter.messagebox.showerror(title= "Erro no login", message = "Usuário ou senha incorretos!")
+
+    def fechar(self):
+        self.janela.destroy()
+
+
+
+    def run(self):
+        self.janela.mainloop()
+
+if __name__ == "__main__":
+    app = Login()
+    app.run()
